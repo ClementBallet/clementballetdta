f5a4cd5b721c25bd17c71106f844767f3db3ef1c
ClementBallet <clementballet@gmail.com>
2017-02-18 17:22:07 +0100

    up projet trello

---
+++ BACK END/PHP/projet trello/view/login.php
@@ -0,0 +1,91 @@
+<div class="container" style="background-color:#EDEFF0;border-radius:10px;">
+  <div class="row">
+    <div class="col-lg-4 col-lg-offset-2">
+      <form>
+        <h2>Créer un compte Trello</h2>
+        <fieldset class="form-group">
+          <label for="exampleInputEmail1">Nom</label>
+          <input type="name" class="form-control" id="exampleInputEmail1" placeholder="Pierre Mortez">
+          <small class="text-muted">We are sure, you've got a good one.</small>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleInputEmail1">E-mail</label>
+          <input type="mail" class="form-control" id="exampleInputEmail1" placeholder="contact@sos-amities.fr">
+          <small class="text-muted">We'll never share your email with anyone else.</small>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </fieldset>
+        <button type="submit" class="btn btn-primary">Créer un nouveau compte</button>
+      </form>
+    </div>
+    <div class="col-lg-4">
+      <form>
+        <h2>Vous avez un compte ?<br>Connectez-vous !</h2>
+        <fieldset class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+          <small class="text-muted">We'll never share your email with anyone else.</small>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleSelect1">Example select</label>
+          <select class="form-control" id="exampleSelect1">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleSelect2">Example multiple select</label>
+          <select multiple class="form-control" id="exampleSelect2">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleTextarea">Example textarea</label>
+          <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+        </fieldset>
+        <fieldset class="form-group">
+          <label for="exampleInputFile">File input</label>
+          <input type="file" class="form-control-file" id="exampleInputFile">
+          <small class="text-muted">This is some placeholder block-level help text for the above input. It's a bit lighter and easily wraps to a new line.</small>
+        </fieldset>
+        <div class="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+            Option one is this and that&mdash;be sure to include why it's great
+          </label>
+        </div>
+        <div class="radio">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+            Option two can be something else and selecting it will deselect option one
+          </label>
+        </div>
+        <div class="radio disabled">
+          <label>
+            <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3" disabled>
+            Option three is disabled
+          </label>
+        </div>
+        <div class="checkbox">
+          <label>
+            <input type="checkbox"> Check me out
+          </label>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </div>
+</div>
---
+++ BACK END/PHP/projet trello/view/navbar.php
@@ -0,0 +1,87 @@
+<nav class="navbar navbar-default" style="background-color:#026AA7;">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">
+        <img src="./ressources/img/header-logo.png" alt="Trello logo" class="logo-head" class="logo-head"/>
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <form class="navbar-form navbar-left">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">
+          <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+        </button>
+      </form>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <span class="glyphicon glyphicon-plus"></span><span class="caret"></span>
+          </a>
+          <ul class="dropdown-menu">
+            <li class="dropdown-header">Créer</li>
+            <li>
+              <a href="#">
+                <li class="dropdown-header">Créer un tableau...<br>
+                 Un tableau est un ensemble de cartes classées<br>
+                 dans une liste de listes. Utilisez-le pour gérer un<br>
+                 projet, une collection ou organiser quoi que ce soit.
+                </li>
+              </a>
+            </li>
+            <li role="separator" class="divider"></li>
+            <li>
+              <a href="#">
+                <li class="dropdown-header">Créer une équipe personnelle...<br>
+                 Une équipe est un groupe de tableaux et de<br>
+                 personnes. Utilisez la pour regrouper des tableaux<br>
+                 dans votre entreprise, équipe ou famille.
+                </li>
+              </a>
+            </li>
+            <li role="separator" class="divider"></li>
+            <li>
+              <a href="#">
+                <li class="dropdown-header">Créer une équipe professionnelle...<br>
+                 Avec Business Class, votre équipe a plus de<br>
+                 sécurité, de contrôles administratifs et des outils<br>
+                 supplémentaires.
+                </li>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Profil<span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li class="dropdown-header">Clément Ballet</li>
+            <li><a href="#">Profil</a></li>
+            <li><a href="#">Paramètres</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#"><span class="glyphicon glyphicon-off"></span> Se déconnecter</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="#">
+            <span class="glyphicon glyphicon-info-sign"></span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <span class="glyphicon glyphicon-bell"></span>
+          </a>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
